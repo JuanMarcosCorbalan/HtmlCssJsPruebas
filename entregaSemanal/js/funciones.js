f01c26418c6a9909c4d5f92eb3d35c05af272961
JuanMarcosCorbalan <jcorbalan@frba.utn.edu.ar>
2024-08-29 14:51:46 -0300

    termino ejemplo

--- entregaSemanal/js/funciones.js
+++ entregaSemanal/js/funciones.js
@@ -1,4 +1,22 @@
-document.getElementById("botonSaludar").addEventListener("click", function(){
+//document.getElementById("botonSaludar").addEventListener("click", function(){
+//    var nombre = document.getElementById("nombre").value;
+//    document.getElementById("saludo").innerHTML = nombre;
+//});
+
+document.getElementById("enviar").addEventListener("click", function(){
     var nombre = document.getElementById("nombre").value;
-    document.getElementById("saludo").innerHTML = nombre;
+    var apellido = document.getElementById("apellido").value;
+    var error = false;
+    if (nombre == "") {
+        document.getElementById("nombre").style.border = "3px solid red"
+        error = true;
+    }
+    if (apellido == "") {
+        document.getElementById("apellido").style.border = "3px solid red"
+        error = true;
+    }
+    if (error == false) {
+        console.log(nombre + " " + apellido);
+    }
+
 });
